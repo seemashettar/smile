812bebb08a9669e5f2e755bd87a36cef206952f8
Hamid R. Tavakoli <hamidreza.tavakoli@gmail.com>
2012-06-15 16:50:35 -0700

    removed unnecessary files

--- webapp/process.php
+++ webapp/process.php
@@ -61,8 +61,6 @@ if( isset($_GET['emotion']) ){
 
 if ( isset( $_GET['stats'] ) ) 
 {
-	if(!isset($_SESSION["login"]["id"])) die("{'message':'authorization error'}");
-
 	$query = "SELECT COUNT(happy) as happy FROM `tavakoli`.`smile_profile` WHERE happy=1;";	
 	$result = mysqli_query($link, $query);
 	$result_fetch =  mysqli_fetch_assoc($result);
@@ -72,11 +70,6 @@ if ( isset( $_GET['stats'] ) )
 	$result_fetch =  mysqli_fetch_assoc($result);
 	$sad = $result_fetch['happy'];
 	echo "{\"happy\":\"$happy\",\"sad\":\"$sad\"}";
-	//unlink($result_fetch["picture"]);
-
-	//$query = "DELETE FROM a4_person WHERE per_id = ".mysqli_real_escape_string($link, $_GET['profileid']);
-	//$result = mysqli_query($link, $query);
-	//header ('Location: members.php');
 }
 
 
